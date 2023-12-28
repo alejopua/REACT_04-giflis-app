403d9bf4d7c8afd3d97c33087ffc7ffbd9574072
alejopua <alejopua@gmail.com>
2023-12-27 22:17:21 -0500

    [chore] (main): Configure main.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// SYTfEMl2VmtCcS8qG7UjGLsGMizMBQtW
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import { GiflisApp } from './GiflisApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GiflisApp />
+  </React.StrictMode>,
+)

eabe114ecb3d1639eac69cbaa4457d576bbee397
alejopua <alejopua@gmail.com>
2023-12-28 09:38:27 -0500

    [feat] (components):  Modify the code logic in the AddCategory component structure.

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
 // eslint-disable-next-line react/prop-types
-export const AddCategory = ({setCategories}) => {
+export const AddCategory = ({onNewCategory}) => {
   const [newValue, setNewValue] = useState('')
 
   const onSubmit = (e) =>{
     e.preventDefault();
-    newValue !== '' ? setCategories((cat) => [...cat, newValue]) : null;
     setNewValue('')
+    newValue !== '' ? onNewCategory(newValue.trim()[0].toUpperCase() + newValue.trim().slice(1)) : null;
   }
 
   return (

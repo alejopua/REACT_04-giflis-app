68c88b6f1b9ff2d72586d3abea3dc14580daffab
alejopua <alejopua@gmail.com>
2023-12-28 15:41:50 -0500

    [feat] (components):  show the titles of the images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import PropTypes from 'prop-types';
 import { getGifs } from '../helpers/getGifs';
+import { useState, useEffect } from 'react';
 
 export const GifGrid = ({category}) => {
+  const [images, setImages] = useState([])
 
-  getGifs(category);
+  const getImages = async() => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
+  useEffect(() => {
+    getImages();
+  }, []);
+  
 
   return (
     <>
       <h2>{category}</h2>
+      <ol>
+        {
+          images.map( ({id, title}) => (
+            <li key={id}>{title}</li>
+          ))
+        }
+      </ol>
     </>
   )
 }

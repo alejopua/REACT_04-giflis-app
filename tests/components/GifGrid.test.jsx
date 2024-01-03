9feafe4ef1cd81f143ac296e7cac76a5051f2d47
alejopua <alejopua@gmail.com>
2024-01-03 16:58:52 -0500

    [test] (GifGrid): complete mock of a custom hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,14 +1,45 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Testing GifGrid.jsx', () => {  
   const category = 'dragon ball'
 
-  test('must show the loading', () => {
+  test('Must show the loading', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
+
     render( <GifGrid category={ category } />)
-    // screen.debug()
     expect( screen.getByText( 'Loading...' ));
     expect( screen.getByText( category ));
+  });
+
+  test('Must display items when loading images from useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      },
+      {
+        id: '123',
+        title: 'Cell',
+        url: 'https://localhost/cell.jpg'
+      }
+    ]
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category={ category } />)
+    expect( screen.getAllByRole('img').length ).toBe( gifs.length );
+
   });
 
 });

46d83f70df5445afa15455da9166b4c470168738
alejopua <alejopua@gmail.com>
2024-01-03 13:59:31 -0500

    [test] (AddCategory): Simulate a form submission

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -4,7 +4,6 @@ import { AddCategory } from "../../src/components/AddCategory";
 describe('Testing in AddCategory.jsx', () => { 
   
   test('must change the value of the text box', () => { 
-
     render( <AddCategory onNewCategory={ () => {}}  />);
     const input = screen.getByRole('textbox');
 
@@ -12,7 +11,21 @@ describe('Testing in AddCategory.jsx', () => {
 
     expect( input.value ).toBe('Saitama');
     // screen.debug();
-  
+  });
+
+  test('Must call onNewCategory if the input has a value.', () => { 
+    const inputValue = 'Saitama';
+    //TODO: ????
+
+    render( <AddCategory onNewCategory={ () => {}}  />);
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+
+    fireEvent.input( input, { target: { value: inputValue } });
+    fireEvent.submit( form );
+    // screen.debug();
+    expect( input.value ).toBe('');
   });
 
 });
